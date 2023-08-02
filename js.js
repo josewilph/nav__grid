9350310e3b71f5a5a9165a4bd655659227538168
josewilph <josewilph@gmail.com>
2023-08-02 00:18:31 -0400

    ahora si

--- js.js
+++ js.js
@@ -1,3 +1,50 @@
+const amburguesa= document.querySelector(".nav__hamburguer");
+
+amburguesa.addEventListener('click', function(){
+    document.getElementsByTagName("html")[0].style.overflow= "hidden";
+    console.log("el boton a sido precionado");
+})
+
+const cerrar= document.querySelector(".nav__close");
+cerrar.addEventListener('click', function(){
+    document.getElementsByTagName("html")[0].style.overflow= "auto";
+    console.log("el boton a sido cerrado");
+})
+
+
+$(function()
+{
+  var flame = $('#flame');
+  var txt = $('h1');
+
+  flame.on(
+  {
+	click: function()
+	{
+	  flame.removeClass('burn').addClass('puff');
+	  $('.smoke').each(function()
+	  {
+		$(this).addClass('puff-bubble');
+	  });
+	  $('#glow').remove();
+
+	  txt.html("Feliz <b>Cumpleaños</b><br> Peña...").delay(2750).fadeOut(300);
+	  $('#candle').animate(
+	  {
+		//'opacity': '.5'
+	  }, 100);
+	  setTimeout(() => {
+		const campo=document.querySelector("#campo");
+		campo.style.display="block";
+		campo.textContent="ya estas vieja...";
+		console.log(campo);
+	  }, 3500);
+	}
+  })   
+});
+
+
+// fuegos artificiales
 // when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
 // not supported in all browsers though and sometimes needs a prefix, so we need a shim
 window.requestAnimFrame = ( function() {
@@ -8,6 +55,7 @@ window.requestAnimFrame = ( function() {
 					window.setTimeout( callback, 1000 / 60 );
 				};
 })();
+
 
 // now we will setup our basic variables for the demo
 var canvas = document.getElementById( 'canvas' ),
